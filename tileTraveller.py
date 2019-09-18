7180b78e817aae6a2a6c210b426be8df321e8de7
Ingvar Óli <ingvaroli97@hotmail.com>
2019-09-18 10:54:18 +0000

    Fallegt

--- tileTraveller.py
+++ tileTraveller.py
@@ -1,16 +1,18 @@
-#Git-hub repository:    https://github.com/IngoLitli/TileTraveller.git
+# Git-hub repository:    https://github.com/IngoLitli/TileTraveller.git
+
 
 def printAvailableMoves(player):
     """Prints out all available moves for the player"""
-    
-    moves = ["(N)orth","(E)ast","(S)outh","(W)est"]
+
+    moves = ["(N)orth", "(E)ast", "(S)outh", "(W)est"]
     availables = []
-    for i in range (4):
+    for i in range(4):
         if board[player[0]][player[1]][i]:
             availables.append(moves[i])
-    
+
     print("You can travel:", end=" ")
     print(" or ".join(availables)+".")
+
 
 def movePlayer(player, move):
     """Moves the player"""
@@ -23,36 +25,39 @@ def movePlayer(player, move):
     elif move == "W":
         player[1] -= 1
 
-    if player != [2,2]:
+    if player != [2, 2]:
         printAvailableMoves(player)
     return player
+
 
 def playerPosition():
     """Returns the playerpostion on board"""
     return (board[player[0]][player[1]])
 
+
 def legalMove(player, move):
     """Checks if the current direciton is a legal move"""
-    Moves = ["N","E","S","W"]
+    Moves = ["N", "E", "S", "W"]
     if playerPosition()[Moves.index(move)]:
         return True
     return False
 
+
 """[N,E,S,W]"""
 board = [
-    [[0,1,1,0], [0,1,0,1], [0,0,1,1]],
-    [[1,1,1,0], [0,0,1,1], [1,0,1,0]],
-    [[1,0,0,0], [1,0,0,0], [1,0,0,0]]
+    [[0, 1, 1, 0], [0, 1, 0, 1], [0, 0, 1, 1]],
+    [[1, 1, 1, 0], [0, 0, 1, 1], [1, 0, 1, 0]],
+    [[1, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0]]
     ]
 
-player = [2,0]
+player = [2, 0]
 
 printAvailableMoves(player)
 
-while player != [2,2]:
+while player != [2, 2]:
     direction = input("Direction: ").upper()
     if legalMove(player, direction):
-        player = movePlayer(player,direction)
+        player = movePlayer(player, direction)
     else:
         print("Not a valid direction!")
 else:
